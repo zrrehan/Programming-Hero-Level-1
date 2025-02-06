49150a1e53975fbbc12815339d00836fb9ba1a1f
Khandaker Ziaur Rahman Rehan <zrrehan@outlook.com>
2025-02-06 21:44:56 +0600

    Started Module 18.5

---
+++ Milestone 4/Module 18.5 (Array Part 1)/0.basic.js
@@ -0,0 +1,8 @@
+let numbers = [10, 20, 30, 40, 50];
+console.log(numbers);
+
+let names = ["Abul", "babul", "Cabul", "dabul", "ebul"];
+console.log(names);
+
+let mixedArray = [12, "Dhaka", true, 45.99];
+console.log(mixedArray);
