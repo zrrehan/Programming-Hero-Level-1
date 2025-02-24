b8eba94a50d2a3bf05ed71844ddc7ca636103c00
Khandaker Ziaur Rahman Rehan <zrrehan@outlook.com>
2025-02-25 00:12:06 +0600

    Added Add Money feature

---
+++ Milestone 5/Module 27 (payo App part 1)/scripts/addMoney.js
@@ -0,0 +1,14 @@
+const button = document.querySelector("#addMoneyButton");
+button.addEventListener("click", function() {
+    let bank = document.querySelector("#bank").value;
+    let pin = document.querySelector("#pin").value;
+
+    if (pin === "1234" && bank !== "notSelected") {
+        let amount = document.querySelector("#Amount").value;
+        amount = parseFloat(amount);
+        let wallet = document.querySelector("#wallet-value"); 
+        wallet.innerText = (parseFloat(wallet.innerText) + amount).toFixed(2);  
+    } else {
+        alert("Select Correct Pin and Bank Account")
+    }
+});
