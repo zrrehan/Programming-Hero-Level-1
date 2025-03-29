cc8b482961c33688dff0ffe68cc2f62af641e077
Khandaker Ziaur Rahman Rehan <zrrehan@outlook.com>
2025-03-29 17:52:27 +0600

    Initialized Ecommerce project of module 41

---
+++ Milestone 7/React Modules (Module 38 - Module 42)/src/Module_41_Modules_Data_Storage_Integration/2.E-commerce-project/Project.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+function Project() {
+    return(
+        <div>
+            <Header></Header>
+        </div>
+    );
+}
+
+export default Project;
---
+++ Milestone 7/React Modules (Module 38 - Module 42)/src/Module_41_Modules_Data_Storage_Integration/Module41.jsx
@@ -0,0 +1,8 @@
+import Project from "./2.E-commerce-project/Project";
+function Module41 () {
+    return(
+        <Project></Project>
+    );
+}
+
+export default Module41;
