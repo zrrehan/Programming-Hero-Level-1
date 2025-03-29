6fc25cf4df2c7d7ef3c4efd4cce2b38bc63edba7
Khandaker Ziaur Rahman Rehan <zrrehan@outlook.com>
2025-03-29 16:51:46 +0600

    Initialized Module 41

---
+++ Milestone 7/React Modules (Module 38 - Module 42)/src/Module_41_Modules_Data_Storage_Integration/1. Import/calculation.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+export {add, substract, multiplication }
---
+++ Milestone 7/React Modules (Module 38 - Module 42)/src/Module_41_Modules_Data_Storage_Integration/1. Import/index.js
@@ -0,0 +1,5 @@
+import { add, substract, multiplication as mul } from "./calculation.js";
+
+console.log(add(2, 3));
+console.log(substract(3, 1));
+console.log(mul(3, 10));
