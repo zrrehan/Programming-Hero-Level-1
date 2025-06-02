5ad757a8c8f226b945d141fda32bb48576e2221e
Khandaker Ziaur Rahman Rehan <zrrehan@outlook.com>
2025-06-02 23:41:56 +0600

    Created Animated GIF with Lottie React

--- Milestone 11/Client/src/Module58_59_Job_Portal/Components/Login.jsx
+++ Milestone 11/Client/src/Module58_59_Job_Portal/Components/Login.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../Context/AuthContext";
+import Lottie from "lottie-react";
+import loginAnimation from "../Assets/loginAnimation.json"
 
 function Login() {
     const { loginFirebase } = useContext(AuthContext);
@@ -13,14 +15,11 @@ function Login() {
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
+                    <Lottie loop={true} animationData={loginAnimation} style={{width: "300px"}}></Lottie>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <div className="card-body">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
                         <form onSubmit = {login} className="fieldset">
                             <label className="label">Email</label>
                             <input type="email" name = "email" className="input" placeholder="Email" />
