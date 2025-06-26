1c632e89c060d127de9d69e1648372fc444f7bad
Khandaker Ziaur Rahman Rehan <zrrehan@outlook.com>
2025-06-26 23:39:37 +0600

    created the navbar and imported all the assets

---
+++ Milestone 12/ProFast/profast-client/src/Components/Layout/Navbar.jsx
@@ -0,0 +1,41 @@
+import { NavLink } from "react-router"
+import logo from "../../Assets/Profast-Logo.png";
+function Navbar() {
+    const links = <>
+        <NavLink to="/">Services</NavLink>
+        <NavLink to="/">Coverage</NavLink>
+        <NavLink to="/">About Us</NavLink>
+        <NavLink to="/">Pricing</NavLink>
+        <NavLink to="/">Be a Rider</NavLink>
+    </>
+    return(
+        <div className="navbar bg-base-100 shadow-sm rounded-2xl mt-8 px-7">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow space-y-3">
+                        {links}
+                    </ul>
+                </div>
+                <a className="h-12 w-[120px] text-xl hidden lg:flex">
+                    <img className="h-full w-full" src={logo} alt="" />
+                </a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 space-x-7">
+                    {links}
+                </ul>
+            </div>
+            <div className="navbar-end space-x-5">
+                <a className="btn rounded-xl px-5 bg-[##eaeced]">Login</a>
+                <a className="btn rounded-xl px-5 bg-[#CAEB66]">Be a rider</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- Milestone 12/ProFast/profast-client/src/Layout.jsx
+++ Milestone 12/ProFast/profast-client/src/Layout.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router";
+import Navbar from "./Components/Layout/Navbar";
+
+
 function Layout() {
     return(
-        <div>
-            hello world
+        <div className="max-w-[1500px] w-[90%] mx-auto">
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <h1>Footer</h1>
         </div>
     )
 }
